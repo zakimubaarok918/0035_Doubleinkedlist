719b1eec6cebd10381bb984e9469ecec06bf1f1f
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-30 21:52:15 +0700

    Implement DoubleLinkedList class structure and add addNode method stub

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -9,3 +9,18 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    
+}
