dcc028913e0c6b0d091739f8715495f6ce248678
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-30 22:20:22 +0700

    Enhance addNode method in DoubleLinkedList to handle node insertion and prevent duplicates

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -48,9 +48,52 @@ public:
             if (START != NULL)
                 START->prev = newNode;
 
-                
+            // step 6: newNode.prev = NULL
+            newNode->prev = NULL;
             
+            // step 7: START = newNode
+            START = newNode;
+            return;
         }
+
+        // insert in between node 
+        // step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != Null && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        // step 9; Insert between current and current->next
+        newNode->next = current->next; // Step 9a: newNode.next = current.next
+        newNode->prev = current;       // Step 9b: newNode.prev = current
+
+        // insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode; // Steo 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode    
+    }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        
     }
 
 }
